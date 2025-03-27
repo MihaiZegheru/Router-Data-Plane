13dd804035e241c96636dd83f08be484753cdfd8
Mihai Zegheru <mihai@mihaizegheru.com>
2025-03-27 13:13:52 +0200

    Added ICMP protocol.
    Signed-off-by: Mihai Zegheru <mihai@mihaizegheru.com>

--- router.c
+++ router.c
@@ -8,6 +8,8 @@
 #define ETHERTYPE_IP		0x0800	/* IP protocol */
 #endif
 
+char cpybuf[100];
+
 /* Routing table */
 struct route_table_entry *route_table;
 int route_table_len;
@@ -19,21 +21,39 @@ int arp_table_len;
 struct packet_data {
 	char *buf;
 	size_t len;
+
+	size_t interface;
+
 	struct ether_hdr *eth_hdr;
 	struct ip_hdr *ip_hdr;
 	struct icmp_hdr *icmp_hdr;
 };
 
-struct packet_data packet_data_init(void *buf, size_t len) {
+struct packet_data packet_data_init(void *buf, size_t len, size_t interface) {
 	struct packet_data pkt = {
 		.buf = buf,
 		.len = len,
+		.interface = interface,
 		.eth_hdr = (struct ether_hdr*)buf,
 		.ip_hdr = (struct ip_hdr *)(buf + sizeof(struct ether_hdr)),
 		.icmp_hdr = NULL
 	};
-
 	return pkt;
+}
+
+void packet_data_insert_icmp_hdr(struct packet_data *pkt) {
+	memcpy(cpybuf, pkt->ip_hdr + sizeof(struct ip_hdr),
+		   sizeof(struct icmp_hdr));
+	memcpy(pkt->ip_hdr + sizeof(struct ip_hdr) + sizeof(struct icmp_hdr),
+		   cpybuf, sizeof(struct icmp_hdr));
+	pkt->icmp_hdr = (struct icmp_hdr *)(pkt->buf + sizeof(struct ether_hdr) +
+										sizeof(struct ip_hdr));
+
+	pkt->ip_hdr->tot_len = htons(ntohs(pkt->ip_hdr->tot_len) +
+								 sizeof(struct icmp_hdr));
+	pkt->ip_hdr->proto = IPPROTO_ICMP;
+
+	pkt->len += sizeof(struct icmp_hdr);
 }
 
 struct route_table_entry *get_best_route(uint32_t ip_dest) {
@@ -75,66 +95,102 @@ void initialise_tables(char* rpath) {
 	arp_table_len = parse_arp_table("arp_table.txt", arp_table);
 }
 
-void handle_icmp(void *buf, uint8_t type, uint8_t code) {
-	struct ether_hdr *eth_hdr = (struct ether_hdr *)buf;
-	struct ip_hdr *ip_hdr = (struct ip_hdr *)(buf + sizeof(struct ether_hdr));
-	struct icmp_hdr *icmp_hdr = (struct icmp_hdr *)(buf +
-													sizeof(struct ether_hdr) +
-													sizeof(struct ip_hdr));
+void handle_icmp(struct packet_data *pkt, uint8_t type, uint8_t code) {
+	if (type) {
+		packet_data_insert_icmp_hdr(pkt);
+	}
+	pkt->icmp_hdr->mtype = type;
+	pkt->icmp_hdr->mcode = code;
 
-	icmp_hdr->mtype = type;
-	icmp_hdr->mcode = code;
-	icmp_hdr->check = 0;
-	icmp_hdr->check = checksum((uint16_t *)icmp_hdr, sizeof(struct icmp_hdr));
+	pkt->ip_hdr->dest_addr = pkt->ip_hdr->source_addr;
+	pkt->ip_hdr->source_addr = inet_addr(get_interface_ip(pkt->interface));
+	pkt->ip_hdr->ttl = 64;
+
+
+	pkt->icmp_hdr->check = 0;
+	pkt->icmp_hdr->check = htons(checksum((uint16_t *)pkt->icmp_hdr,
+										  pkt->len - sizeof(struct ether_hdr) - 
+										  sizeof(struct ip_hdr)));
+	pkt->ip_hdr->checksum = 0;
+	pkt->ip_hdr->checksum = htons(checksum((uint16_t *)pkt->ip_hdr,
+										   sizeof(struct ip_hdr)));
+
+	/* Update Ethernet header */
+	memcpy(pkt->eth_hdr->ethr_dhost, pkt->eth_hdr->ethr_shost, 6);
+	/* Get source MAC address */
+	get_interface_mac(pkt->interface, pkt->eth_hdr->ethr_shost);
+	pkt->eth_hdr->ethr_type = htons(ETHERTYPE_IP);
+
+	/* Send the packet */
+	int res = send_to_link(pkt->len, pkt->buf, pkt->interface);
+
+	printf("Package sent therough ICMP. %d\n", res);
 }
 
-void handle_ipv4(struct packet_data pkt) {
+int check_icmp_for_self(struct packet_data *pkt) {
+	if (pkt->ip_hdr->dest_addr == inet_addr(get_interface_ip(pkt->interface))) {
+		pkt->icmp_hdr = (struct icmp_hdr *)(pkt->buf + 
+											sizeof(struct ether_hdr) +
+											sizeof(struct ip_hdr));
+		return 1;
+	}
+	return 0;
+}
+
+void handle_ipv4(struct packet_data *pkt) {
 	printf("Handling IPv4 packet.\n");
 
-	/* Check if we got an ICMP packet. */
-	if (pkt.ip_hdr->proto == IPPROTO_ICMP) {
-
-	}
-
 	/* Verify IP checksum */
-	if (checksum((uint16_t *)pkt.ip_hdr, sizeof(struct ip_hdr)) != 0) {
+	if (checksum((uint16_t *)pkt->ip_hdr, sizeof(struct ip_hdr)) != 0) {
 		printf("Dropping packet: invalid checksum\n");
 		return;
 	}
 
+	/* Check if we got an ICMP packet. */
+	if (pkt->ip_hdr->proto == IPPROTO_ICMP && check_icmp_for_self(pkt)) {
+		if (pkt->icmp_hdr->mtype == 8 && pkt->icmp_hdr->mcode == 0) {
+			handle_icmp(pkt, 0, 0);
+			return;
+		}
+	}
+
 	/* Find the best route. Drop packet if fail. */
-	struct route_table_entry* route = get_best_route(pkt.ip_hdr->dest_addr);
+	struct route_table_entry* route = get_best_route(pkt->ip_hdr->dest_addr);
 	if (!route) {
 		printf("Dropping packet: No route found\n");
+		handle_icmp(pkt, 3, 0);
 		return;
 	}
 
 	/* Check TTL */
-	if (pkt.ip_hdr->ttl <= 1) {
-		printf("Dropping packet: TTL expired\n");
+	if (pkt->ip_hdr->ttl <= 1) {
+		printf("Dropping packet: TTL expired\n");\
+		handle_icmp(pkt, 11, 0);
 		return;
 	}
-	pkt.ip_hdr->ttl--;
+	pkt->ip_hdr->ttl--;
 
 	/* Recompute checksum */
-	pkt.ip_hdr->checksum = 0;
-	pkt.ip_hdr->checksum = htons(checksum((uint16_t *)pkt.ip_hdr, sizeof(struct ip_hdr)));
+	pkt->ip_hdr->checksum = 0;
+	pkt->ip_hdr->checksum = htons(checksum((uint16_t *)pkt->ip_hdr,
+										   sizeof(struct ip_hdr)));
 
 	/* Get destination MAC address (static ARP table) */
 	struct arp_table_entry* dest_mac = get_arp_entry(route->next_hop);
 	if (!dest_mac) {
-		printf("Dropping packet: MAC entry not found for IP %u\n", route->next_hop);
+		printf("Dropping packet: MAC entry not found for IP %u\n",
+			   route->next_hop);
 		return;
 	}
 
 	/* Get source MAC address */
-	get_interface_mac(route->interface, pkt.eth_hdr->ethr_shost);
+	get_interface_mac(route->interface, pkt->eth_hdr->ethr_shost);
 	
 	/* Update Ethernet header */
-	memcpy(pkt.eth_hdr->ethr_dhost, dest_mac->mac, 6);
+	memcpy(pkt->eth_hdr->ethr_dhost, dest_mac->mac, 6);
 
 	/* Send the packet */
-	int res = send_to_link(pkt.len, pkt.buf, route->interface);
+	int res = send_to_link(pkt->len, pkt->buf, route->interface);
 
 	printf("Package forwarded. %d\n", res);
 }
@@ -163,11 +219,11 @@ int main(int argc, char *argv[]) {
 
 		printf("Packet received!\n");
 
-		struct packet_data pkt = packet_data_init(buf, len);
+		struct packet_data pkt = packet_data_init(buf, len, interface);
 
 		/* Check if we got an IPv4 packet, else drop packet. */
 		if (ntohs(pkt.eth_hdr->ethr_type) == ETHERTYPE_IP) {
-			handle_ipv4(pkt);
+			handle_ipv4(&pkt);
 		} else {
 			printf("Ignored non-IPv4 packet\n");
 			continue;
